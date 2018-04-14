8ed96ed513cfa1ff1012ec98059117ef0c9c9d71
Masami Akizuki <34865695+Masami45@users.noreply.github.com>
2018-04-15 00:22:13 +0700

    Create stop.js

---
+++ commands/stop.js
@@ -0,0 +1,98 @@
+const cfg = require("../config.json");
+const Discord = require("discord.js");
+const key = process.env.YT_API;
+const fs = require("fs"); 
+const moment = require('moment');
+const yt = require('ytdl-core');
+const YouTube = require('simple-youtube-api');
+const youtube = new YouTube(key);
+const opus = require("opusscript");
+const gyp = require("node-gyp");
+
+exports.run = async(music, message, args, color,  queue) => { 
+    const args1 = message.content.split(' ');
+  const searchString = args1.slice(1).join(' ');
+  const url = args1[1] ? args1[1].replace(/<(.+)>/g, '$1') : '';
+  const serverQueue = queue.get(message.guild.id); 
+ 
+    if (!message.member.voiceChannel) return message.channel.send('You are not in a voice channel!');
+    if (!serverQueue) return message.channel.send("There isn't anything for me to stop for you");
+    serverQueue.songs = [];
+    serverQueue.connection.dispatcher.end('Stop command has been used!');
+    message.channel.send(":stop_button: Music has been stopped!");
+    return undefined; 
+
+    // Time for the functions
+
+async function handleVideo(video, message, voiceChannel, playlist = false) {
+  const serverQueue = queue.get(message.guild.id);
+  console.log(video);
+  const song = {
+    id: video.id,
+    title: video.title,
+    url: `https://www.youtube.com/watch?v=${video.id}`
+  };
+  if (!serverQueue) {
+    const queueConstruct = {
+      textChannel: message.channel,
+      voiceChannel: voiceChannel,
+      connection: null,
+      skippers: [],
+      songs: [],
+      volume: 5,
+      playing: true
+    };
+    queue.set(message.guild.id, queueConstruct);
+
+    queueConstruct.songs.push(song);
+
+    try {
+      var connection = await voiceChannel.join();
+      queueConstruct.connection = connection;
+      play(message.guild, queueConstruct.songs[0]);
+    } catch (error) {
+      console.error(`I could not join the voice channel: ${error}`);
+      queue.delete(message.guild.id);
+      return message.channel.send(`I could not join the voice channel: ${error}`);
+    }
+  } else {
+    serverQueue.songs.push(song);
+    console.log(serverQueue.songs);
+    if (playlist) return undefined;
+    else return message.channel.send(`✅ **${song.title}** has been added to the queue!`);
+  }
+  return undefined;
+}
+
+function play(guild, song) {
+  const serverQueue = queue.get(guild.id);
+
+  if (!song) {
+    serverQueue.voiceChannel.leave();
+    queue.delete(guild.id);
+    return;
+  }
+  console.log(serverQueue.songs);
+
+const dispatcher = serverQueue.connection.playStream(yt(song.url))
+        .on('end', reason => {
+            if (reason === 'Stream is not generating quickly enough.') console.log('Song ended.');
+            else console.log(reason);
+            serverQueue.songs.shift();
+            setTimeout(() => {
+                play(guild, serverQueue.songs[0]);
+            }, 250);
+        })
+        .on('error', error => console.error(error));
+    dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+
+    serverQueue.textChannel.send(`🎶 Start playing: **${song.title}**`);
+}
+}
+
+exports.help = {
+    name: "stop",
+    description: "Stops the music playing and removes all the songs from the queue",
+    usage: "pr!stop",
+    note: "Removes all the song from the queue"
+}
