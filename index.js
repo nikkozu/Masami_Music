fc3cca1df81b2b4e7934f75fc3e52ff3ef609e9d
Masami Akizuki <34865695+Masami45@users.noreply.github.com>
2018-06-11 23:57:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
 const Discord = require("discord.js");
-const cfg = require("./config.json");
 const music = new Discord.Client({disableEveryone: true});
-const fs = require("fs");
 music.commands = new Discord.Collection();
 const {color} = require('./config.json');
 const queue = new Map();
 
 music.on('message', async message => {
-    let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
-	  if (!prefixes[message.guild.id]) {
-		prefixes[message.guild.id] = {
-			prefixes: cfg.prefix
-		};
-    }
 
-    let prefix = prefixes[message.guild.id].prefixes;
+    let prefix = '!';
     let msg = message.content.toLowerCase();
-    let sender = message.author;
     let args = message.content.slice(prefix.length).trim().split(" ");
     let cmd = args.shift().toLowerCase();
+
+    if (msg == `<@${music.user.id}>` || msg == `<@!${music.user.id}>`) {
+        message.reply(`My prefix is ${prefix}`);
+    }
 
     if (!msg.startsWith(prefix)) return;
     if (sender.bot) return;
@@ -30,7 +25,7 @@ music.on('message', async message => {
         console.log(e.message); 
     } finally { 
         console.log(`${message.author.username} ran the command: ${cmd} on ${message.guild.name}`);
-	}
+    }
 });
 
 music.login(process.env.TOKEN);
